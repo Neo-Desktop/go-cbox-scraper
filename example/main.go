11cee3336d217feb2b64230edb1d7fc9f349dadf
Amrit Panesar <me@amrit.pw>
2020-10-29 15:12:49 -0700

    add additional note to example program

--- example/main.go
+++ example/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	cboxscraper ".."
+	"log"
+)
+
 func main() {
-	$END$
+	// this example configuration is for a "new" style cbox
+	// this will not work with the program as-is
+	info := cboxscraper.CBoxServerInfo{
+		WebHostID: 6,
+		BoxID:     850801,
+		BoxTag:    "hD3VIj",
+		Debug:     true,
+	}
+
+	scraper := cboxscraper.NewScraper(info, -1, -1)
+
+	err := scraper.Load("test.gob")
+	if err != nil {
+		log.Println("Load failed:", err)
+	}
+
+	err = scraper.Scrape(true)
+	if err != nil {
+		log.Println("Scraper failed:", err)
+	}
+
+	err = scraper.Save("test.gob")
+	if err != nil {
+		log.Println("Save failed:", err)
+	}
 }
